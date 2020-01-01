a7dd7090ef4a9f3213513b2bb7ed1685327a6e90
Danilo <dnlmcrd@gmail.com>
2020-01-01 14:46:10 -0300

    Try android

--- android/app/jni/Android.mk
+++ android/app/jni/Android.mk
@@ -1,53 +1,43 @@
 LOCAL_PATH := $(call my-dir)
 
-sdl_libs=$(LOCAL_PATH)/SDL2/build/android/lib
-
-ENG_DIR := $(LOCAL_PATH)/../../../engine
+SDL_LIBS := $(LOCAL_PATH)/SDL2/build/android/lib
 SRC_DIR := $(LOCAL_PATH)/../../../src
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := hidapi
-LOCAL_SRC_FILES := $(sdl_libs)/$(TARGET_ARCH_ABI)/libhidapi.so
+LOCAL_SRC_FILES := $(SDL_LIBS)/$(TARGET_ARCH_ABI)/libhidapi.so
 include $(PREBUILT_SHARED_LIBRARY)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := SDL2
-LOCAL_SRC_FILES := $(sdl_libs)/$(TARGET_ARCH_ABI)/libSDL2.so
+LOCAL_SRC_FILES := $(SDL_LIBS)/$(TARGET_ARCH_ABI)/libSDL2.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := SDL2_image
-LOCAL_SRC_FILES := $(sdl_libs)/$(TARGET_ARCH_ABI)/libSDL2_image.so
+LOCAL_SRC_FILES := $(SDL_LIBS)/$(TARGET_ARCH_ABI)/libSDL2_image.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := mpg123
-LOCAL_SRC_FILES := $(sdl_libs)/$(TARGET_ARCH_ABI)/libmpg123.so
+LOCAL_SRC_FILES := $(SDL_LIBS)/$(TARGET_ARCH_ABI)/libmpg123.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := SDL2_mixer
-LOCAL_SRC_FILES := $(sdl_libs)/$(TARGET_ARCH_ABI)/libSDL2_mixer.so
+LOCAL_SRC_FILES := $(SDL_LIBS)/$(TARGET_ARCH_ABI)/libSDL2_mixer.so
 include $(PREBUILT_SHARED_LIBRARY)
 
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := PisteEngine
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/SDL2/include $(LOCAL_PATH)/SDL2_image $(LOCAL_PATH)/SDL2_mixer
-LOCAL_SRC_FILES := $(wildcard $(ENG_DIR)/*.cpp)
-LOCAL_CPPFLAGS += -std=c++17 -Wno-write-strings
-LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer hidapi mpg123
-include $(BUILD_STATIC_LIBRARY)
 
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := PK2
-LOCAL_C_INCLUDES := $(ENG_DIR) $(SRC_DIR)
-LOCAL_SRC_FILES := $(wildcard $(SRC_DIR)/*.cpp)
-LOCAL_CPPFLAGS += -std=gnu++17 -Wno-write-strings
-LOCAL_STATIC_LIBRARIES := PisteEngine
+LOCAL_C_INCLUDES := $(SRC_DIR)
+LOCAL_SRC_FILES := $(wildcard $(SRC_DIR)/*.cpp) $(wildcard $(SRC_DIR)/*/*.cpp)
+LOCAL_CPPFLAGS += -std=gnu++17
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer hidapi mpg123
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
 include $(BUILD_SHARED_LIBRARY)
+
--- android/app/src/main/java/org/pgnapps/pk2/PK2Activity.java
+++ android/app/src/main/java/org/pgnapps/pk2/PK2Activity.java
@@ -1,12 +1,18 @@
 package org.pgnapps.pk2;
 
 import org.libsdl.app.SDLActivity;
+import org.libsdl.app.SDL;
 
 import android.content.pm.ActivityInfo;
 import android.os.Build;
-import android.view.View;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.util.Log;
+
+import java.io.IOException;
+import java.lang.Object;
+import java.lang.reflect.Method;
 
 public class PK2Activity extends SDLActivity {
     private static final String LOG_TAG = "PK2Activity";
@@ -21,9 +27,89 @@ public class PK2Activity extends SDLActivity {
         }
     }
 
+    private static Method getEntriesMethod;
+    private static Method expansionFileMethod;
+    private static Object expansionFile;
+
+    // This method is called by SDL using JNI.
+    public static String[] Utils_ScanAPKdir(String dir) throws IOException {
+        
+        if (expansionFile == null) {
+            String mainHint = nativeGetHint("SDL_ANDROID_APK_EXPANSION_MAIN_FILE_VERSION");
+            if (mainHint == null) {
+                return null; // no expansion use if no main version was set
+            }
+            String patchHint = nativeGetHint("SDL_ANDROID_APK_EXPANSION_PATCH_FILE_VERSION");
+            if (patchHint == null) {
+                return null; // no expansion use if no patch version was set
+            }
+
+            Integer mainVersion;
+            Integer patchVersion;
+            try {
+                mainVersion = Integer.valueOf(mainHint);
+                patchVersion = Integer.valueOf(patchHint);
+            } catch (NumberFormatException ex) {
+                ex.printStackTrace();
+                throw new IOException("No valid file versions set for APK expansion files", ex);
+            }
+
+            try {
+                // To avoid direct dependency on Google APK expansion library that is
+                // not a part of Android SDK we access it using reflection
+                expansionFile = Class.forName("com.android.vending.expansion.zipfile.APKExpansionSupport")
+                    .getMethod("getAPKExpansionZipFile", Context.class, int.class, int.class)
+                    .invoke(null, SDL.getContext(), mainVersion, patchVersion);
+
+                expansionFileMethod = expansionFile.getClass()
+                    .getMethod("getInputStream", String.class);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                expansionFile = null;
+                expansionFileMethod = null;
+                throw new IOException("Could not access APK expansion support library1", ex);
+            }
+        }
+
+        if (getEntriesMethod == null) {
+            try {
+                getEntriesMethod = expansionFile.getClass()
+                    .getMethod("getEntriesAt", String.class);
+            } catch (Exception ex) {
+                getEntriesMethod = null;
+                throw new IOException("Could not access APK expansion support library2", ex);
+            }
+        }
+
+
+        Object[] list = null;
+
+        try {
+            list = expansionFileMethod.invoke(expansionFile, dir);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new IOException("Could not open stream from APK expansion file3", ex);
+        }
+
+        if (list == null) {
+            // calling "getEntriesAt" was successful but null was returned
+            throw new IOException("Could not find path in APK expansion file4");
+        }
+
+        for (Object n : list) {
+
+            //Log.v("PK2", "Found: " + n.mFileName);
+
+        }
+
+        return new String[] {"a", "b"}; //list;
+
+    }
+
     @Override
     protected String[] getArguments() {
         return new String[0];
+        //return new String[] { "dev", "test", "rooster island 1/map001.map" };
     }
 
     @Override
@@ -45,13 +131,16 @@ public class PK2Activity extends SDLActivity {
 
     @Override
     protected void onStart() {
-        makeFullScreen();
+        Utils_ScanAPKdir("sprites/");
+        //makeFullScreen();
         super.onStart();
+        setWindowStyle(true);
     }
 
     @Override
     protected void onResume() {
-        makeFullScreen();
+        //makeFullScreen();    
         super.onResume();
+        setWindowStyle(true);
     }
 }
